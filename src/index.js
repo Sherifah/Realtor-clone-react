16741d91864925eb1a2cf0285c37e0e63bc05551
Sherifah <adelekebolaji0@gmail.com>
2023-10-28 09:35:21 +0100

    created pages and routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css'
 
 import App from './App';
 
-import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById('root')).render(<App />)
 // ReactDOM.render(<App />, document.getElementById('root') )
